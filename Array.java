910fa39532e1c6cb9ab670b93c66b4ef3a892c9c
Makarand6969 <125957482+Makarand6969@users.noreply.github.com>
2023-02-27 10:09:33 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Array
+{
+ public static void main(String[]args)
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter size");
+ int size=sc.nextInt();
+ int[]a=new int[size];
+{
+ for(int i=0;i<a.length;i++)
+{
+ a[i]=sc.nextInt();
+}
+ for(int i=0;i<a.length;i++)
+{
+ int c=1;
+ if(i!=a.length) 
+{
+ if(a[i]!=0)
+{
+ for(int j=i+1;j<a.length;j++)
+{
+ if(a[i]==a[j])
+{
+ a[j]=0;
+ c++;
+}
+}
+ System.out.println("count of :"+a[i]+"="+c);
+}
+}
+}
+}
+}
+}
